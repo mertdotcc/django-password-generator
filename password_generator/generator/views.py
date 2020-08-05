f73958c4801b366701801f57495e25f8da6c96eb
Mert <mert1ozturk@gmail.com>
2020-08-05 20:37:36 +0300

    first prot

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+import random
 
-# Create your views here.
+
+def home(request):
+
+    context = {
+        'weak_range': range(6,16),
+        'strong_range': range(16,129),
+        'unbelievable_range': [256,512,1024,2048],
+    }
+
+    return render(request, 'generator/home.html', context)
+
+
+def password(request):
+
+    lower_case_letters = list('abcdefghijklmnopqrstuvwxyz')
+    upper_case_letters = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    numbers = list('0123456789')
+    symbols = list('!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~')
+
+    length = int(request.GET.get('length',16))
+
+    available_chars = lower_case_letters
+
+    if request.GET.get('uppercase'):
+        available_chars += upper_case_letters
+
+    if request.GET.get('numbers'):
+        available_chars += numbers
+
+    if request.GET.get('symbols'):
+        available_chars += symbols
+    
+    password = ''
+    for l in range(length):
+        password += random.choice(available_chars)
+
+    return render(request, 'generator/password.html', {'password':password})
--- password_generator/password_generator/settings.py
+++ password_generator/password_generator/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd Party
+    'crispy_forms',
+
+    # Apps
+    'generator',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
